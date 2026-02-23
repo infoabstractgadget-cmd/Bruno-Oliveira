7576b4b5f198871c83605021b3740b54055176a6
infoabstractgadget-cmd <info.abstractgadget@gmail.com>
2026-02-23 17:49:02 +0000

    feat: Update DEFAULT_METRICS with Sumol Compal benchmark data

--- constants.ts
+++ constants.ts
@@ -4,14 +4,14 @@ export const MONTHS = ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Ou
 
 export const PLATFORMS = ["Instagram","Facebook","TikTok"];
 
-export const PLAT_COLORS: { [key: string]: string } = { 
-  Instagram: "#E1306C", 
-  Facebook: "#1877F2", 
-  TikTok: "#00f2ea" 
+export const PLAT_COLORS: { [key: string]: string } = {
+  Instagram: "#E1306C",
+  Facebook: "#1877F2",
+  TikTok: "#00f2ea"
 };
 
 export const DEFAULT_METRICS: Metrics = {
-  Instagram: { cpm: 0.47, cpe: 0.16, cpc: 1.08, er: 1.03, ctr: 0.03 },
-  Facebook: { cpm: 0.43, cpe: 0.25, cpc: 0.62, er: 0.51, ctr: 0.07 },
-  TikTok: { cpm: 0.74, cpe: 0.28, cpc: 0.63, er: 0.53, ctr: 0.08 },
+  Instagram: { cpm: 1.22, cpe: 0.26, cpc: 0.63, er: 0.50, ctr: 0.10 },
+  Facebook: { cpm: 0.65, cpe: 0.55, cpc: 0.19, er: 0.18, ctr: 0.06 },
+  TikTok: { cpm: 1.34, cpe: 0.34, cpc: 0.92, er: 0.44, ctr: 0.24 },
 };
